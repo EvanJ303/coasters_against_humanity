d8477c02334bc696f43aa0a68e39dccfde8166af
Evan Johnson <ecrjmegamanx@gmail.com>
2024-07-10 11:07:40 -0400

    done

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let board=Array.from(document.getElementsByClassName("gridimage"));
-let act_array=[]
-let playstop=0
+let act_array=[];
+let playstop=0;
+let gridcon=document.getElementById("")
 
 for(let i=0=0;i<64;i++){
-    document.getElementById()
+    document.getElementById("container_grid").createElement(div,class="gridimage");
+    
 }
 
 for(let i=0;i<64;i++){
-    act_array.push(0)
+    act_array.push(0);
 }
 
 while(playstop==0){
     for(let i=0;i<64;i++)
         if(act_array[i]==1){
-            document.getElement
+            document.getElement;
         }
 }
