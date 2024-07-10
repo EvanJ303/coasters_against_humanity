754fb6c1acfd8100d4666b907f2ba7a283d517e6
Evan Johnson <ecrjmegamanx@gmail.com>
2024-07-10 10:01:46 -0400

    fuck

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ let board=Array.from(document.getElementsByClassName("gridimage"));
 let act_array=[]
 let playstop=0
 
-for (let i=0;i<64;i++){
+for(let i=0=0;i<64;i++){
+    document.getElementById()
+}
+
+for(let i=0;i<64;i++){
     act_array.push(0)
 }
 
 while(playstop==0){
-    if plays
+    for(let i=0;i<64;i++)
+        if(act_array[i]==1){
+            document.getElement
+        }
 }
